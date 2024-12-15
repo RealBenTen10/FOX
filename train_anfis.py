ba49463b356b4215f8509b722ec0a71b98bead0f
RealBenTen10 <dabenedi@hu-berlin.de>
2024-12-15 15:34:34 +0100

    changes to encoding and parameters

--- train_anfis.py
+++ train_anfis.py
@@ -8,35 +8,53 @@ from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
 import numpy as np
 import pandas as pd
 import pickle as pk
-seed = 123
-np.random.seed(seed)
 import torch
-torch.manual_seed(seed)
 
 # Set the encoding type here: choose 'boolean', 'label', 'index', or 'one_hot'
-encoding_type = 'one_hot'
+# Available encodings:
+encoding_types = [
+    'boolean',
+    'label',
+    'index',
+    'one_hot'
+]
+# pick the encoding of your choice
+encoding_type = encoding_types[1]
 
 # Change the dataset_name to create a new trained model (.h5 file)
-dataset_name = 'sepsis_cases_1'
 # Available datasets:
-# 'sepsis_cases_1'
-# 'sepsis_cases_2'
-# 'sepsis_cases_4'
-# 'bpic2011_f1'
-# 'bpic2011_f2'
-# 'bpic2011_f3'
-# 'bpic2011_f4'
-# 'bpic2012_accepted'
-# 'bpic2012_declined'
-# 'bpic2012_cancelled'
-# 'production'
+dataset_names = [
+    'sepsis_cases_1',
+    'sepsis_cases_2',
+    'sepsis_cases_4',
+    'bpic2011_f1',
+    'bpic2011_f2',
+    'bpic2011_f3',
+    'bpic2011_f4',
+    'bpic2012_accepted',
+    'bpic2012_declined',
+    'bpic2012_cancelled',
+    'production'
+]
+# pick the dataset of your choice
+dataset_name = dataset_names[0]
 
+# Set some parameters
+# Set the number of epochs
+epochs = 10
+sigmoid = True  # use sigmoid instead of softmax
+test_size = 0.2
+random_state = 69
+batch_size = 250
+learning_rate = 0.001
 
+# not used?
 seed = 123
 np.random.seed(seed)
 torch.manual_seed(seed)
 
 class ClassifierDataset(Dataset):
+
     def __init__(self, X_data, y_data):
         self.X_data = X_data
         self.y_data = y_data
@@ -66,11 +84,16 @@ def index_encoding(data):
     encoded = np.array([indices[val] for val in data])
     return torch.tensor(encoded, dtype=torch.long)
 
-def one_hot_encoding(data, num_categories):
+def one_hot_encoding(data, num_categories, dtype=torch.float):
     """Convert data into one-hot representation."""
-    data_tensor = torch.tensor(data, dtype=torch.long).unsqueeze(1)
-    one_hot = torch.zeros(len(data), num_categories, dtype=torch.float).scatter(1, data_tensor, 1)
-    return one_hot
+    num_entries = len(data)
+    # Convert data to a torch tensor of indices, with extra dimension:
+    cats = torch.Tensor(data).long().unsqueeze(1)
+    # Now convert this to one-hot representation:
+    y = torch.zeros((num_entries, num_categories), dtype=dtype) \
+        .scatter(1, cats, 1)
+    y.requires_grad = True
+    return y
 
 def get_data(dataset, n_feature, batch_size, columns_sel):
     dataframe = pd.read_csv(f'dataset/{dataset}/{dataset}_train.csv', header=0, sep=',')
@@ -82,7 +105,7 @@ def get_data(dataset, n_feature, batch_size, columns_sel):
     d_target = array[:, -1]
 
     X_train, X_val, y_train, y_val = train_test_split(
-        d_data, d_target, test_size=0.2, stratify=d_target, random_state=69, shuffle=True
+        d_data, d_target, test_size=test_size, stratify=d_target, random_state=random_state, shuffle=True
     )
 
     # Encode targets based on the chosen encoding type
@@ -97,40 +120,37 @@ def get_data(dataset, n_feature, batch_size, columns_sel):
         y_train_encoded = one_hot_encoding(y_train, num_categories)
     else:
         raise ValueError(f"Unsupported encoding type: {encoding_type}")
+    print("Encoding_type: " + encoding_type)
 
-    train_dataset = ClassifierDataset(torch.tensor(X_train, dtype=torch.float), y_train_encoded)
-    val_dataset = ClassifierDataset(torch.tensor(X_val, dtype=torch.float), torch.tensor(y_val, dtype=torch.long))
+    train_dataset = ClassifierDataset(torch.from_numpy(X_train).float(), torch.from_numpy(y_train).long())
+    val_dataset = ClassifierDataset(torch.from_numpy(X_val).float(), torch.from_numpy(y_val).long())
 
-    return (
-        DataLoader(train_dataset, batch_size=batch_size, shuffle=True),
-        DataLoader(val_dataset, batch_size=batch_size),
-        columns_sel
-    )
+    x = torch.Tensor(X_train)
+    y = y_train_encoded
+    td = TensorDataset(x, y)
+
+    return DataLoader(train_dataset, batch_size=batch_size, shuffle=False), DataLoader(val_dataset, batch_size=batch_size), DataLoader(td, batch_size=batch_size, shuffle=False), columns_sel
 
 def train(dataset, n_feature, learning_rate, bs, columns_sel):
-    train_data, val_data, columns_sel = get_data(dataset, n_feature, bs, columns_sel)
-    x_train, y_train = next(iter(train_data))
-
-    model = make_anfis(x_train, num_mfs=3, num_out=len(torch.unique(y_train)), hybrid=False)
+    train_data, val_data, x, columns_sel = get_data(dataset, n_feature, bs, columns_sel)
+    x_train, y_train = x.dataset.tensors
+    model = make_anfis(x_train, num_mfs=3, num_out=2, hybrid=False)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    model, score = experimental.train_anfis_cat(model, train_data, val_data, optimizer, 100)
-    torch.save(model, 'streamlit_fox/models/model_' + dataset + '.h5')
+    model, score = experimental.train_anfis_cat(model, train_data, val_data, optimizer,epochs, encoding_type, sigmoid)
     torch.save(model, 'models/model_' + dataset + '.h5')
+    torch.save(model, 'streamlit_fox/models/model_' + dataset + '.h5')
     load_model.metrics(dataset, columns_sel)
     return model
-
 def opt(dataset, n_feature, learning_rate, bs, file_name, columns_sel):
-    train_data, val_data, columns_sel = get_data(dataset, n_feature, bs, columns_sel)
-    x_train, y_train = next(iter(train_data))
+    train_data, val_data, x, columns_sel = get_data(dataset, n_feature, bs, columns_sel)
+    x_train, y_train = x.dataset.tensors
 
-    model = make_anfis(x_train, num_mfs=3, num_out=len(torch.unique(y_train)), hybrid=False)
+    model = make_anfis(x_train, num_mfs=3, num_out=2, hybrid=False)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-    model, scores = experimental.train_anfis_cat(model, train_data, val_data, optimizer, 100)
+    model, scores = experimental.train_anfis_cat(model, train_data, val_data, optimizer,epochs, encoding_type, sigmoid)
     return model, scores
 
-
+dataset_name = 'sepsis_cases_1'
 
 if dataset_name == 'sepsis_cases_1':
         columns_sel = ['Diagnose', 'mean_open_cases', 'Age', 'std_Leucocytes', 'std_CRP']#sepsis_cases_1
@@ -157,4 +177,5 @@ elif dataset_name == 'production':
 
 params = pk.load(open('params/'+dataset_name+".p", "rb"))
 n_features = len(columns_sel)
-model = train(dataset_name, n_features, params.get('lr'), params.get('batch_size'), columns_sel[:n_features])
+# model = train(dataset_name, n_features, params.get('lr'), params.get('batch_size'), columns_sel[:n_features])
+model = train(dataset_name, n_features, learning_rate, batch_size, columns_sel[:n_features])
