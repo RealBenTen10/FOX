59ff3b431b3b2aba7e02ebbd04ce63eac0aadf46
RealBenTen10 <dabenedi@hu-berlin.de>
2024-12-15 15:36:22 +0100

    added activation function

--- load_model.py
+++ load_model.py
@@ -1,37 +1,37 @@
 import torch
 import numpy as np
 import pandas as pd
-from sklearn.metrics import f1_score, roc_auc_score
+from sklearn.metrics import f1_score, roc_auc_score, recall_score, precision_score
 import os
 from torch.autograd import Variable
 import plotly.graph_objects as go
 
-
 os.environ['TF_FORCE_GPU_ALLOW_GROWTH'] = 'true'
+
 
 def metrics(dataset_name, columns_sel):
     model = torch.load('models/model_' + dataset_name + '.h5')
 
-    pd_len = pd.read_csv("dataset/"+dataset_name+"/len_test"+dataset_name+".csv", header=0, sep=',')
+    pd_len = pd.read_csv("dataset/" + dataset_name + "/len_test" + dataset_name + ".csv", header=0, sep=',')
     max_len = pd_len['Len'].max()
 
-    df_test = pd.read_csv("dataset/"+dataset_name+"/"+dataset_name+'_test.csv', header=0, sep=',')
+    df_test = pd.read_csv("dataset/" + dataset_name + "/" + dataset_name + '_test.csv', header=0, sep=',')
     df_test = df_test[columns_sel]
-    
+
     image_all = df_test.values
     image_all = image_all[:, 0:len(df_test.columns) - 1]
 
-    y_test = pd.read_csv("dataset/"+dataset_name+"/"+dataset_name+"_test.csv")
+    y_test = pd.read_csv("dataset/" + dataset_name + "/" + dataset_name + "_test.csv")
     y_test = y_test[y_test.columns[-1]]
 
-    f = open(dataset_name+"_results.csv", "w")
-    f.write("SELECTED COLUMNS;"+"\n")
+    f = open(dataset_name + "_results.csv", "w")
+    f.write("SELECTED COLUMNS;" + "\n")
     for element in columns_sel:
         f.write(element + "\n")
-    f.write("----------------;"+"\n")
+    f.write("----------------;" + "\n")
 
-    f.write("LENGHT;NUMBEROFSAMPLES;ROC_AUC_SCORE"+"\n")
-    
+    f.write("LENGHT;NUMBEROFSAMPLES;ROC_AUC_SCORE" + "\n")
+
     weights = []
     list_index_len = []
     preds_all = []
@@ -40,6 +40,8 @@ def metrics(dataset_name, columns_sel):
     i = 0
     auc_list = []
     f1_score_list = []
+    recall_list = []
+    precision_list = []
     index_len = 1
 
     while i < max_len:
@@ -55,17 +57,21 @@ def metrics(dataset_name, columns_sel):
         conv_test = np.asarray(image_test)
         conv_test_for_check = np.asarray(image_test)
         conv_test = torch.tensor(conv_test, dtype=torch.float)
-        
+
         pred = model(torch.Tensor(conv_test))
         pred2 = torch.argmax(pred, 1)
 
         pred = pred.detach().numpy()
         pred2 = pred2.detach().numpy()
- 
+
         if len(target_test) == 1:
             print("skip")
         else:
-            f.write(str(index_len)+";"+str(len(target_test))+";"+str(roc_auc_score(target_test, pred[:, 1]))+"\n")
+            print("Inside: " + str(index_len) + ";" + str(len(target_test)) + ";" + str(
+                roc_auc_score(target_test, pred[:, 1])) + "\n")
+            f = open(dataset_name + "_results.csv", "w")
+            f.write("" + str(index_len) + ";" + str(len(target_test)) + ";" + str(
+                roc_auc_score(target_test, pred[:, 1])) + "\n")
             auc_list.append(roc_auc_score(target_test, pred[:, 1]))
             f1_score_list.append(f1_score(target_test, pred2))
             list_index_len.append(index_len)
@@ -74,26 +80,24 @@ def metrics(dataset_name, columns_sel):
 
         i = i + 1
 
-
     auc_list = np.asarray(auc_list)
     weights = np.asarray(weights)
 
-    auc_weight = np.sum((auc_list * weights))/np.sum(weights)
-    f1_weight = np.sum((f1_score_list * weights))/np.sum(weights)
+    auc_weight = np.sum((auc_list * weights)) / np.sum(weights)
+    f1_weight = np.sum((f1_score_list * weights)) / np.sum(weights)
 
     print("---WEIGHTED METRICS---")
     print("ROC_AUC_SCORE weighted: %.2f" % auc_weight)
 
-    f.write('ROC_AUC_SCORE weighted;'+str(auc_weight)+";"+"\n")
-    f.write('F1_SCORE weighted;'+str(f1_weight)+";"+"\n")
+    f.write('ROC_AUC_SCORE weighted;' + str(auc_weight) + ";" + "\n")
+    f.write('F1_SCORE weighted;' + str(f1_weight) + ";" + "\n")
     f.close()
-
 
     fig = go.Figure()
     fig.add_trace(go.Scatter(
         x=list_index_len,
         y=auc_list,
-        name = dataset_name,
+        name=dataset_name,
         connectgaps=True
     ))
 
@@ -105,4 +109,4 @@ def metrics(dataset_name, columns_sel):
             color="#7f7f7f"
         )
     )
-    #fig.show()
+    # fig.show()
